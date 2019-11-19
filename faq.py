e49f0e2db51c20dcdd9c66cbc40e3fe5c34fde0f
Naveen <naveen.213@gmail.com>
2019-11-19 15:17:42 +0530

    Modified faqs for integration

--- faq.py
+++ faq.py
@@ -28,14 +28,12 @@ class createFAQ(Resource):
         {
             "CharityID": 1, 
             "EventID":  "12",
-            "UserID": 25,
             "QueryString": "Will refreshments be provided at the event?"
         }
         '''
         req = eval(request.data)
         cid = req["CharityID"]
         eid = req["EventID"]
-        uid = req["UserID"]
 
         events = db["events"]
         # Check if corresponding charity and event IDs exist
@@ -50,17 +48,17 @@ class createFAQ(Resource):
             new_faq_id = new_faq_id["FaqID"]
             new_faq_id += 1
         query = req["QueryString"]
-        faqs.insert_one({"CharityID": cid, "EventID": eid, "FaqID" : new_faq_id, "QueryOwnerID": uid,  "QueryString": query, "Replies" : []})
+        faqs.insert_one({"CharityID": cid, "EventID": eid, "FaqID" : new_faq_id, "QueryString": query, "Answer" : "", "Answered":0})
         return "New FAQ Query added with FaqID %s"%(new_faq_id), 201
 
 
-api.add_resource(createFAQ, '/charity/event/initThread')
+api.add_resource(createFAQ, '/charity/event/initQuery')
 
 # ------------------------------------------------------
 
 # Adding replies to existing thread
 
-class replyFAQ(Resource):
+class answerFAQ(Resource):
     def post(self):
         '''
         Expected json format:
@@ -68,188 +66,126 @@ class replyFAQ(Resource):
             "CharityID": 1, 
             "EventID":  "12",
             "FaqID": 3,
-            "ReplyString": "No, participants are kindly requested to bring their own refreshments.",
-            "UserID": 54
+            "Answer": "No, participants are kindly requested to bring their own refreshments."
         }
-        *** Changes required: UserID of the collaborators to be added to the collaborators sub-collection of events collection
         '''
         req = eval(request.data)
         cid = req["CharityID"]
         eid = req["EventID"]
         fid = req["FaqID"]
-        uid = req["UserID"]
         faqs = db["faqs"]
         # Check if corresponding charity, event IDs and FAQ IDs exist
         faq = faqs.find_one({"CharityID": cid, "EventID": eid, "FaqID":fid})
         if(faq == None):
             return "There is no FAQ ID %s and event ID %s for charity ID %s in DB"%(fid, eid, cid), 404
-        # Check if answer is by event coordinator. If yes, then mark reply as highlighted answer
-        event = db["events"].find_one({"CharityID": cid, "EventID": eid})
-        is_answer = 0
-        if(any(d["UserID"] == uid for d in event['Coordinators'])):
-            is_answer = 1
-        # Check if any replies exist for this event. If yes, get max value of ++ReplyID as new ReplyID
-        if(len(faq["Replies"]) == 0):
-            new_reply_id = 0
-        else:
-            seq = [x["ReplyID"] for x in faq["Replies"]]
-            new_reply_id = max(seq) + 1
-        reply = req["ReplyString"]
-        new_replies = faq["Replies"]
-        new_replies.append({"ReplyID": new_reply_id, "ReplyString": reply, "isAnswer" : is_answer, "Replier": uid})
-        faqs.update_one({"CharityID": cid, "EventID": eid, "FaqID":fid}, {"$set" : {"Replies" : new_replies}})
-        return "New reply added with ReplyID %s"%(new_reply_id), 201
+        '''Uncomment when sessions are active'''
+        # if (session["logged_in"] == False):
+        #     return "Not logged in", 400
 
-api.add_resource(replyFAQ, '/charity/event/replyThread')
-
-# ------------------------------------------------------
-
-# Modifying/deleting replies to existing thread
-
-class modifyFAQ(Resource):
-    def put(self):
-        '''
-        Expected json format:
-        {
-            "CharityID": 1, 
-            "EventID":  "12",
-            "FaqID": 3,
-            "ReplyString": "No, participants are kindly requested to bring their own refreshments.",
-            "ReplyID": 4,
-            "UserID" : 23
-        }
-        if replyID == -1, replyString will contain replacement value for QueryString
-        '''
-        req = eval(request.data)
-        cid = req["CharityID"]
-        eid = req["EventID"]
-        fid = req["FaqID"]
-        rid = req["ReplyID"]
-        new_reply = req["ReplyString"]
-        faqs = db["faqs"]
-        # Check if corresponding charity, event IDs and FAQ IDs exist
-        faq = faqs.find_one({"CharityID": cid, "EventID": eid, "FaqID":fid})
-        if(faq == None):
-            return "There is no FAQ ID %s and event ID %s for charity ID %s in DB"%(fid, eid, cid), 404
-
-        else:
-            if(rid != -1):
-                modified_reply = faq["Replies"]
-                flag = 0
-                for i in range(len(modified_reply)):
-                    if(modified_reply[i]["ReplyID"] == rid):
-                        modified_reply[i]["ReplyString"] = new_reply
-                        flag = 1
-                if(flag):
-                    faqs.update_one({"CharityID": cid, "EventID": eid, "FaqID":fid}, {'$set': {"Replies" : modified_reply}})        
-                    return "Reply modified", 200
-                else:
-                    return "There is no reply ID %s for FAQ ID %s,event ID %s and charity ID %s in DB"%(rid, fid, eid, cid), 404
-            else:
-                faqs.update_one({"CharityID": cid, "EventID": eid, "FaqID":fid}, {'$set':{"QueryString" : new_reply}})
-                return "Updated", 200
-    def delete(self):
-        '''
-        Expected json format: 
-        {
-            "CharityID": 1, 
-            "EventID":  "12",
-            "FaqID": 3,
-            "ReplyID": 4,
-            "DeleteThread": 0
-        }
-
-        DeleteThread = 0 if only the specified reply is to be deleted. Else DeleteThread = 1, and ReplyID sent as -1
-        '''
-        req = eval(request.data)
-        cid = req["CharityID"]
-        eid = req["EventID"]
-        fid = req["FaqID"]
-        rid = req["ReplyID"]
-        isDelete = req["DeleteThread"]
-        faqs = db["faqs"]
-        # Check if corresponding charity, event IDs and FAQ IDs exist
-        faq = faqs.find_one({"CharityID": cid, "EventID": eid, "FaqID":fid})
-        if(faq == None):
-            return "There is no FAQ ID %s and event ID %s for charity ID %s in DB"%(fid, eid, cid), 404
-        else:
-            if(isDelete):
-                faqs.delete_one({"CharityID": cid, "EventID": eid, "FaqID": fid})
-                return "Thread succesfully deleted", 200
-            else:
-                new_replies = faq["Replies"]
-                for i in range(len(new_replies)):
-                    if(new_replies[i]["ReplyID"] == rid):
-                        new_replies.remove(new_replies[i])
-                        faqs.update_one({"CharityID": cid, "EventID": eid, "FaqID": fid}, {"$set" : {"Replies": new_replies}})
-                        return "Reply succesfully deleted", 200
-
-                return "There is no Reply ID %s for FAQ ID %s,event ID %s and charity ID %s in DB"%(rid,fid, eid, cid), 404
-                    
+        # if(session["logged_in_as"] != "Charity" or session["cid"] != cid):
+        #     return "The cid does not match or you are not a charity!", 400
         
+        answer = req["Answer"]
+        faqs.update_one({"CharityID": cid, "EventID": eid, "FaqID":fid}, {"$set" : {"Answer" : answer}})
+        faqs.update_one({"CharityID": cid, "EventID": eid, "FaqID":fid}, {"$set" : {"Answered" : 1}})
 
-api.add_resource(modifyFAQ, '/charity/event/modifyFAQ')
+        return "New answer", 201
+
+api.add_resource(answerFAQ, '/charity/event/answerQuery')
+
 # ------------------------------------------------------
 
 
 # Get full thread
 
-class getFAQ(Resource):
+class getAnsweredFAQ(Resource):
     def get(self):
         '''
         Expected json format:
         {
             "CharityID": 1, 
-            "EventID":  "12",
-            "FaqID": 3,
+            "EventID":  "12"
         }
 
         returned json format:
-        {
-            "Query": 
-                    {
-                        "content": "Will participants recieve certificates for finishing event?",
-                        "owner": 23
-                    }
-            "Replies":
-                    [
-                        {
-                            "content" : "Yes, they will! Register soon!",
-                            "owner": 25
-                            "isAnswer": 1
-                        },
-                        {
-                            "content" : "I wanted to know this as well, please do reply coordinators!",
-                            "owner": 29
-                            "isAnswer": 0
-                        }
-                    ]
-        }
-
+        [
+            {
+                "Query": Will participants recieve certificates for finishing event?",                   
+                "Answer":"Yes, they will! Register soon!"
+            }
+            {
+                "Query": Why does this event not help cats?",                   
+                "Answer":"Because dogs are better than cats"
+            }
+        ]
         '''
         req = eval(request.data)
         cid = req["CharityID"]
         eid = req["EventID"]
-        fid = req["FaqID"]
         faqs = db["faqs"]
 
         # Check if corresponding charity, event IDs and FAQ IDs exist
-        faq = faqs.find_one({"CharityID": cid, "EventID": eid, "FaqID":fid})
+        faq = faqs.find({"CharityID": cid, "EventID": eid, "Answered": 1})
         if(faq == None):
-            return "There is no FAQ ID %s and event ID %s for charity ID %s in DB"%(fid, eid, cid), 404
-        else:
-            ret_val = {}
-            ret_val["Query"] = {"content": faq["QueryString"], "owner": faq["QueryOwnerID"]}
-            ret_val["Replies"] = []
-            
-            for i in faq["Replies"]:
-                ret_val["Replies"].append({"content": i["ReplyString"], "owner": i["Replier"], "isAnswer": i["isAnswer"]})
-            return ret_val, 200
+            return "There is no answered FAQs for event ID %s for charity ID %s in DB"%(eid, cid), 204
+        ret_val = []
+        for i in faq:
+            temp = {}
+            temp["Query"] = i["QueryString"]
+            temp["Answer"] = i["Answer"]
+            ret_val.append(temp)            
+        return ret_val, 200
 
             
 
 
-api.add_resource(getFAQ, '/charity/event/getFAQ')
+api.add_resource(getAnsweredFAQ, '/charity/event/getAnsweredFAQ')
+
+
+
+class getunAnsweredFAQ(Resource):
+    def get(self):
+        '''
+        Expected json format:
+        {
+            "CharityID": 1, 
+            "EventID":  "12"
+        }
+
+        returned json format:
+        [        
+            {
+                "Query":"Will participants recieve certificates for finishing event?",
+                "FaqID": 2
+            },  
+            {
+                "Query": "Why does this event not help cats?"     
+                "FaqID": 2
+            }      
+        ]
+        '''
+        req = eval(request.data)
+        cid = req["CharityID"]
+        eid = req["EventID"]
+        faqs = db["faqs"]
+
+        # Check if corresponding charity, event IDs and FAQ IDs exist
+        faq = faqs.find({"CharityID": cid, "EventID": eid, "Answered": 0})
+        if(faq == None):
+            return "There is no answered FAQs for event ID %s for charity ID %s in DB"%(eid, cid), 204
+        ret_val = []
+        for i in faq:
+            temp = {}
+            temp["Query"] = i["QueryString"]
+            temp["FaqID"] = i["FaqID"]
+            ret_val.append(temp)            
+        return ret_val, 200
+
+
+            
+
+
+api.add_resource(getunAnsweredFAQ, '/charity/event/getunAnsweredFAQ')
 
 # ------------------------------------------------------
 
