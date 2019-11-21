47def7450a09df9f8c69857b2503507fd52b451b
Naveen <naveen.213@gmail.com>
2019-11-21 12:35:02 +0530

    modified collection and key names, and added getuserprofile page

--- faq.py
+++ faq.py
@@ -1,6 +1,7 @@
 from flask import Flask, jsonify, request, session 
 from flask_restful import Resource, Api
 from pymongo import MongoClient
+import datetime
 
 # ------------------------------------------------------
 
@@ -37,7 +38,7 @@ class createFAQ(Resource):
 
         events = db["events"]
         # Check if corresponding charity and event IDs exist
-        if(events.find_one({"CharityID": cid, "EventID": eid}) == None):
+        if(events.find_one({"cid": cid, "eid": eid}) == None):
             return "There is no event ID %s for charity ID %s in DB"%(eid, cid), 404
         faqs = db["faqs"]
         # Check if any faqs exist for this event. If yes, get max value of ++FaqID as new FaqID
@@ -62,12 +63,7 @@ class answerFAQ(Resource):
     def post(self):
         '''
         Expected json format:
-        {
-            "CharityID": 1, 
-            "EventID":  "12",
-            "FaqID": 3,
-            "Answer": "No, participants are kindly requested to bring their own refreshments."
-        }
+        {   "CharityID": 1, "EventID":  "12","FaqID": 3,"Answer": "No, participants are kindly requested to bring their own refreshments."}
         '''
         req = eval(request.data)
         cid = req["CharityID"]
@@ -182,10 +178,62 @@ class getunAnsweredFAQ(Resource):
         return ret_val, 200
 
 
-            
+
 
 
 api.add_resource(getunAnsweredFAQ, '/charity/event/getunAnsweredFAQ')
+
+
+
+# ------------------------------------------------------
+
+
+class getUserPage(Resource):
+    def get(self):
+        '''    
+        Expected json format:
+            {
+                "UserID": "USER53537173"
+            }
+            returned json format:
+                
+            {
+                "email":"mayankrao16@gmail.com",
+                "eventsList":
+                [
+                    {
+                        "charityName": "Ninaada Foundation",
+                        "eventTitle": "Raising money for orphaned children in Bangalore:,
+                        "eventDate": "22 November, 2019"
+                    },
+                    {
+                        "charityName": "Bro Foundation",
+                        "eventTitle": "Raising money for orphaned bros in Bangalore:,
+                        "eventDate": "21 November, 2019"
+                    }
+                ]
+            }
+        '''
+        req = eval(request.data)
+        user_c = db['user_details']
+        events = db['events']
+        res = user_c.find_one({"uid": req['UserID']})
+        eventsList = []
+        for i in res['registered-events']:
+            print("eid", i)
+            temp = events.find_one({'eid':i})
+            print("Temp: ", temp)
+            temp_dict = {"charityName":temp['foundation-name'], 'eventTitle':temp['event-title'], 'eventDate':temp['date']}
+            eventsList.append(temp_dict)
+        for i in eventsList:
+            i['eventDate'] = datetime.datetime.strptime(i['eventDate'], '%a %b %d %Y %X GMT%z (%Z)')
+        
+        eventsList = sorted(eventsList, key = lambda i:i['eventDate'], reverse = True)
+        response = jsonify({"email":res['email'], "eventsList":eventsList})
+        response.status_code = 200
+        return response
+
+api.add_resource(getUserPage, '/user/details')
 
 # ------------------------------------------------------
 
