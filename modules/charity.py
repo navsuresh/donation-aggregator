cd8d34d994344ee756b502d29fbf06b0741796a2
Mayankrao-16 <55399795+Mayankrao-16@users.noreply.github.com>
2019-11-22 03:54:39 +0530

    Upgraded widgets, Charity storage and reading added, more functionality to some pages

--- modules/charity.py
+++ modules/charity.py
@@ -8,8 +8,27 @@ api = Api(api_bp)
 
 
 class CustomProfilePage(Resource):
-    def get(self):
+    def get(self,cid):
         return globals.app.send_static_file('customize.html')
 
+class GetCustomCharityData(Resource):
+    def post(self,cid):
+        data = request.get_json()
+        pageData = globals.db.charities.find_one({"cid": cid})
+        if pageData==None:
+            globals.db.charities.insert(data)
+        else:
+            globals.db.charities.update({"cid": cid}, {"$set": data})
+        response = "Added"
+        return response
 
-api.add_resource(CustomProfilePage, '/profile')
+class LoadCharityPageData(Resource):
+    def post(self,cid):
+        pageData = globals.db.charities.find_one({"cid": cid})
+        del pageData['_id']
+        response = jsonify(pageData)
+        return response
+
+api.add_resource(CustomProfilePage, '/profile/<cid>')
+api.add_resource(GetCustomCharityData, '/customdata/<cid>')
+api.add_resource(LoadCharityPageData, '/charitydata/<cid>')
--- modules/events.py
+++ modules/events.py
@@ -1,10 +1,10 @@
 from modules import globals
 from uuid import uuid1
-from flask import request, jsonify, Response, session, Blueprint, render_template
+from flask import request, jsonify, Response, session, Blueprint, render_template, make_response
 from flask_restful import Resource, Api
 from modules.search import Search
 from modules.notification import push_notification
-
+import random
 
 api_bp = Blueprint('api_events', __name__)
 api = Api(api_bp)
@@ -168,10 +168,41 @@ class GetWidget(Resource):
 
 
 class Calendar(Resource):
-    def get(self):
+    def get(self,eid):
         return globals.app.send_static_file('calendar.html')
+    
+class Followers(Resource):
+    def get(self, eid):
+        return make_response("<h2 style='background-color: powderblue'> Participants:</h2><h1>" + str(len(globals.db.events.find_one({'eid': eid})["participants"])) + "</h1>",200,{'Content-Type': 'text/html'})
 
+class Quote(Resource):
+    
+    def get(self, eid):
+        return globals.app.send_static_file('quote.html')
 
+class QuoteData(Resource):
+    quoteno = 0
+    def get(self):
+        
+        quotes = [
+            "It's not how much we give but how much love we put into giving.#-Mother Theresa",
+            "There is no exercise better for the heart than reaching down and lifting people up.#-John Holmes",
+            "When we give cheerfully and accept gratefully, everyone is blessed.#-Maya Angelou",
+            "A bone to the dog is not charity. Charity is the bone shared with the dog, when you are just as hungry as the dog.#-Jack London",
+            "When I give food to the poor, they call me a saint. When I ask why the poor have no food, they call me a communist.#-Dom Helder Camara, Dom Helder Camara: Essential Writings",
+            "While we do our good works let us not forget that the real solution lies in a world in which charity will have become unnecessary.#-Chinua Achebe, Anthills of the Savannah"
+        ]
+        QuoteData.quoteno=(QuoteData.quoteno+1)%len(quotes)
+        return  quotes[QuoteData.quoteno]
+
+class EventTitle(Resource):
+    def get(self, eid):
+        return make_response("<h2 style='background-color: lightgreen'> Title: <br>" + str(globals.db.events.find_one({'eid': eid})["event-title"]) + "</h2>",200,{'Content-Type': 'text/html'})
+
+api.add_resource(EventTitle, '/<eid>/eventtitle')
+api.add_resource(Quote, '/<eid>/quote')
+api.add_resource(QuoteData, '/quotedata')
+api.add_resource(Followers, '/<eid>/followers')
 api.add_resource(UpdateWidget, '/<eid>/update-widget')
 api.add_resource(GetWidget, '/<eid>/get-widget')
 api.add_resource(CreateEvent, '/<cid>/create-event')
@@ -183,7 +214,7 @@ api.add_resource(UpdateEvent, '/<eid>/update-event')
 api.add_resource(Search, '/search/<query>')
 api.add_resource(Event, '/event-listings')
 api.add_resource(EventsPage, '/events/<eventid>')
-api.add_resource(Calendar, '/calendar')
+api.add_resource(Calendar, '/<eid>/calendar')
 
 #
 # if __name__ == '__main__':
